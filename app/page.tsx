7edfef21adde4ae5d41984e6774629e59ffeeb21
Origamiccc <82864201+Origamiccc@users.noreply.github.com>
2024-11-17 07:27:55 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,37 @@
-// pages/index.js
+"use client";
+import { NavigationLayout } from "@/components/NavigationLayout";
 import { PayBlock } from "@/components/Pay";
 import { SignIn } from "@/components/SignIn";
 import { VerifyBlock } from "@/components/Verify";
 import { SignMessageBlock } from "@/components/SignMessage";
 import { SendTransBlock } from "@/components/SendTrans";
-import Layout from "@/components/Layout";
+import { useState } from "react";
 
 export default function Home() {
+  const [currentTab, setCurrentTab] = useState("home");
+
+  const renderContent = () => {
+    switch (currentTab) {
+      case "home":
+        return <SignIn />;
+      case "verify":
+        return <VerifyBlock />;
+      case "pay":
+        return <PayBlock />;
+      case "sign":
+        return <SignMessageBlock />;
+      case "send":
+        return <SendTransBlock />;
+      default:
+        return <SignIn />;
+    }
+  };
+
   return (
-    <Layout>
-      <div className="grid grid-cols-1 gap-4">
-        <div id="sign-in">
-          <SignIn />
-        </div>
-        <div id="verify">
-          <VerifyBlock />
-        </div>
-        <div id="pay">
-          <PayBlock />
-        </div>
-        <div id="sign-message">
-          <SignMessageBlock />
-        </div>
-        <div id="send-transaction">
-          <SendTransBlock />
-        </div>
+    <NavigationLayout currentTab={currentTab} setCurrentTab={setCurrentTab}>
+      <div className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center p-6">
+        {renderContent()}
       </div>
-    </Layout>
+    </NavigationLayout>
   );
 }
