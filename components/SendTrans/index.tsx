c02dd0bc2df02f413f2873ca6d8d98702680b626
JINYANG <ruhigeswl@outlook.com>
2024-11-17 03:07:24 +0800

    Update: UPdate send transaction.

--- components/SendTrans/index.tsx
+++ components/SendTrans/index.tsx
@@ -6,93 +6,70 @@ import { createPublicClient, http } from 'viem'
 import { worldchain } from 'viem/chains'
 import { useEffect, useState } from "react";
 
-const sendTransactionCommand = async () => {
-    // const deadline = Math.floor((Date.now() + 30 * 60 * 1000) / 1000).toString()
-
-    // // Transfers can also be at most 1 hour in the future.
-    // const permitTransfer = {
-    //     permitted: {
-    //         token: "ScoinToken",
-    //         amount: '1',
-    //     },
-    //     nonce: Date.now().toString(),
-    //     deadline,
-    // }
-
-    // const permitTransferArgsForm = [
-    //     [permitTransfer.permitted.token, permitTransfer.permitted.amount],
-    //     permitTransfer.nonce,
-    //     permitTransfer.deadline,
-    // ]
-
-    // const transferDetails = {
-    //     to: '0x126f7998Eb44Dd2d097A8AB2eBcb28dEA1646AC8',
-    //     requestedAmount: '1',
-    // }
-
-    // const transferDetailsArgsForm = [transferDetails.to, transferDetails.requestedAmount]
-
-    const payload = await MiniKit.commandsAsync.sendTransaction({
-        transaction: [
-            {
-                address: '0x74acA102a5968D2CA320E6aa091b670D90c70Ac0',
-                abi: [
-                    {
-                      "inputs": [
-                        {
-                          "internalType": "int256",
-                          "name": "_initialInt",
-                          "type": "int256"
-                        }
-                      ],
-                      "stateMutability": "nonpayable",
-                      "type": "constructor"
-                    },
-                    {
-                      "inputs": [],
-                      "name": "getInt",
-                      "outputs": [
-                        {
-                          "internalType": "int256",
-                          "name": "",
-                          "type": "int256"
-                        }
-                      ],
-                      "stateMutability": "view",
-                      "type": "function"
-                    },
-                    {
-                      "inputs": [],
-                      "name": "storedInt",
-                      "outputs": [
-                        {
-                          "internalType": "int256",
-                          "name": "",
-                          "type": "int256"
-                        }
-                      ],
-                      "stateMutability": "view",
-                      "type": "function"
-                    }
-                  ],
-                functionName: 'getInt',
-                args: [],
-            },
-        ]
-        // permit2: [
-        //     {
-        //         ...permitTransfer,
-        //         spender: '0x34afd47fbdcc37344d1eb6a2ed53b253d4392a2f',
-        //     },
-        // ],
-        
-    })
-    console.log(payload)
-}
-
 export const SendTransBlock = () => {
 
+    const sendTransactionCommand = async () => {
+        // const deadline = Math.floor((Date.now() + 30 * 60 * 1000) / 1000).toString()
+    
+        // // Transfers can also be at most 1 hour in the future.
+        // const permitTransfer = {
+        //     permitted: {
+        //         token: "ScoinToken",
+        //         amount: '1',
+        //     },
+        //     nonce: Date.now().toString(),
+        //     deadline,
+        // }
+    
+        // const permitTransferArgsForm = [
+        //     [permitTransfer.permitted.token, permitTransfer.permitted.amount],
+        //     permitTransfer.nonce,
+        //     permitTransfer.deadline,
+        // ]
+    
+        // const transferDetails = {
+        //     to: '0x126f7998Eb44Dd2d097A8AB2eBcb28dEA1646AC8',
+        //     requestedAmount: '1',
+        // }
+    
+        // const transferDetailsArgsForm = [transferDetails.to, transferDetails.requestedAmount]
+    
+        const { commandPayload, finalPayload } = await MiniKit.commandsAsync.sendTransaction({
+            transaction: [
+                {
+                    address: '0x288e9BC8b8b0A534ba477B2e98B0951BF6eC2375',
+                    abi: [
+                        {
+                            "inputs": [
+                              {
+                                "internalType": "int256",
+                                "name": "_newInt",
+                                "type": "int256"
+                              }
+                            ],
+                            "name": "setInt",
+                            "outputs": [],
+                            "stateMutability": "nonpayable",
+                            "type": "function"
+                          },
+                      ],
+                    functionName: 'setInt',
+                    args: [5],
+                },
+            ]
+            // permit2: [
+            //     {
+            //         ...permitTransfer,
+            //         spender: '0x34afd47fbdcc37344d1eb6a2ed53b253d4392a2f',
+            //     },
+            // ],
+            
+        })
+        console.log(commandPayload, finalPayload)
+    }
+
     const [transactionId, setTransactionId] = useState<string>('')
+
     const client = createPublicClient({
         chain: worldchain,
         transport: http('https://worldchain-mainnet.g.alchemy.com/public'),
